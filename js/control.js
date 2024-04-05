7614f5e0f12fb2740edead927603898f475f00fc
krakowi <165930588+krakowi@users.noreply.github.com>
2024-04-05 14:46:21 +0300

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,324 @@
+let secondWindow = null; // Переменная для хранения ссылки на второе окно
+let timerRunning = false; // Переменная для хранения состояния таймера
+let timerInterval;
+let seconds = 90;
+let AkaVisible = false;
+let AoVisible = false;
+var akaScore = 0;
+var aoScore = 0;
+var akaippon = 0;
+var akawazaari = 0;
+var akayuko = 0;
+var aoippon = 0;
+var aowazaari = 0;
+var aoyuko = 0;
+var audioPlayer = document.getElementById('audioPlayer');
+audioPlayer.muted = true;
+var end = document.getElementById('end-game');
+end.muted = true;
+
+document.getElementById("settings-button").addEventListener("click", function() {
+    document.getElementById("time-modal").style.display = "block";
+});
+
+document.getElementsByClassName("close-time")[0].addEventListener("click", function() {
+    document.getElementById("time-modal").style.display = "none";
+});
+
+document.getElementById("tatami-button").addEventListener("click", function() {
+    document.getElementById("tatami-modal").style.display = "block";
+});
+
+document.getElementsByClassName("close-tatami")[0].addEventListener("click", function() {
+    document.getElementById("tatami-modal").style.display = "none";
+});
+
+function uncheckAllCheckboxes() {
+    // Получаем все чекбоксы на странице
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // Проходимся по каждому чекбоксу и снимаем отметку
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+}
+
+function warn(checkbox) {
+    // Если чекбокс отмечен, делаем элемент видимым, иначе скрываем его
+    if (secondWindow && !secondWindow.closed) {
+        if (checkbox.checked) {
+            secondWindow.postMessage({newWarn: checkbox.id}, '*');
+        } else {
+            secondWindow.postMessage({unWarn: checkbox.id}, '*');
+        }
+    }
+}
+
+function openOrFocusSecondWindow() {
+    if (secondWindow === null || secondWindow.closed) {
+        secondWindow = window.open("scoreboard.html", "_blank", "width=1600,height=900");
+        secondWindow.timerRunning = timerRunning; // Передаем состояние таймера во второе окно
+    } else {
+        secondWindow.focus();
+    }
+}
+
+function minusScore(color) {
+    if (color === 'akayuko') {
+        if (akayuko >= 1) {
+            if (akayuko === 1) {
+                document.getElementById("akayukocount").setAttribute("disabled", "disabled");
+            }
+            akayuko--;
+            akaScore--;
+        }
+    } else if (color === 'aoyuko') {
+        if (aoyuko >= 1) {
+            if (aoyuko === 1) {
+                document.getElementById("aoyukocount").setAttribute("disabled", "disabled");
+            }
+            aoyuko--;
+            aoScore--;
+        }
+    } else if (color === 'akawazaari') {
+        if (akawazaari >= 1) {
+            if (akawazaari === 1) {
+                document.getElementById("akawazaaricount").setAttribute("disabled", "disabled");
+            }
+            akawazaari--;
+            akaScore -= 2;
+        }
+    } else if (color === 'aowazaari') {
+        if (aowazaari >= 1) {
+            if (aowazaari === 1) {
+                document.getElementById("aowazaaricount").setAttribute("disabled", "disabled");
+            }
+            aowazaari--;
+            aoScore -= 2;
+        }
+    } else if (color === 'akaippon') {
+        if (akaippon >= 1) {
+            if (akaippon === 1) {
+                document.getElementById("akaipponcount").setAttribute("disabled", "disabled");
+            }
+            akaippon--;
+            akaScore -= 3;
+        }
+    } else if (color === 'aoippon') {
+        if (aoippon >= 1) {
+            if (aoippon === 1) {
+                document.getElementById("aoipponcount").setAttribute("disabled", "disabled");
+            }
+            aoippon--;
+            aoScore -= 3;
+        }
+    }
+    updateScore()
+    updatecount()
+}
+
+function yuko(color) {
+    if (color === 'aka') {
+        akaScore++;
+        akayuko++;
+        document.getElementById("akayukocount").removeAttribute("disabled");
+    } else if (color === 'ao') {
+        aoScore++;
+        aoyuko++;
+        document.getElementById("aoyukocount").removeAttribute("disabled");
+    }
+    updateScore()
+    updatecount()
+}
+
+function wazaari(color) {
+    if (color === 'aka') {
+        akaScore += 2;
+        akawazaari++;
+        document.getElementById("akawazaaricount").removeAttribute("disabled");
+    } else if (color === 'ao') {
+        aoScore += 2;
+        aowazaari++;
+        document.getElementById("aowazaaricount").removeAttribute("disabled");
+    }
+    updateScore()
+    updatecount()
+}
+
+function ippon(color) {
+    if (color === 'aka') {
+        akaScore += 3;
+        akaippon++;
+        document.getElementById("akaipponcount").removeAttribute("disabled");
+    } else if (color === 'ao') {
+        aoScore += 3;
+        aoippon++;
+        document.getElementById("aoipponcount").removeAttribute("disabled");
+    }
+    updateScore()
+    updatecount()
+}
+
+function updatecount() {
+    document.getElementById('aoippon').textContent = "IPPON⠀⠀⠀"+aoippon;
+    document.getElementById('aowazaari').textContent = "WAZA-ARI "+aowazaari;
+    document.getElementById('aoyuko').textContent = "YUKO ⠀⠀⠀"+aoyuko;
+    
+    document.getElementById('akaippon').textContent = "IPPON⠀⠀⠀"+akaippon;
+    document.getElementById('akawazaari').textContent = "WAZA-ARI "+akawazaari;
+    document.getElementById('akayuko').textContent = "YUKO ⠀⠀⠀"+akayuko;
+}
+
+function updateScore() {
+    document.getElementById('aka-score').innerText = akaScore;
+    document.getElementById('ao-score').innerText = aoScore;
+    
+    var data = {
+        newAkaScore: akaScore,
+        newAoScore: aoScore
+    }
+
+    if (secondWindow && !secondWindow.closed) {
+        secondWindow.postMessage({newScore: data}, '*');
+    }
+}
+
+function startTimer() {
+    if (!timerRunning) {
+        timerInterval = setInterval(updateTimer, 1000);
+        document.getElementById('timerDisplay').style.color = 'rgb(255, 187, 0)';
+        document.getElementById('stop-button').style.display = 'block';
+        document.getElementById('start-button').style.display = 'none';
+    }
+    timerRunning = true;
+    if (secondWindow && !secondWindow.closed) {
+        secondWindow.postMessage('start', '*');
+    }
+    audioPlayer.play()
+    end.play()
+}
+
+function pauseTimer() {
+    timerRunning = false;
+    if (secondWindow && !secondWindow.closed) {
+        secondWindow.postMessage('pause', '*');
+    }
+    clearInterval(timerInterval);
+    document.getElementById('stop-button').style.display = 'none';
+    document.getElementById('start-button').style.display = 'block';
+    document.getElementById('timerDisplay').style.color = 'white';
+    audioPlayer.muted = true;
+    end.muted = true;
+}
+
+function formatTime(time) {
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = time % 60;
+
+    return `${minutes < 10 ? minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+}
+
+function resetMatch() {
+    pauseTimer()
+    seconds = 90;
+    akaScore = 0;
+    aoScore = 0;
+    document.getElementById('timerDisplay').innerText = formatTime(seconds);
+    if (secondWindow && !secondWindow.closed) {
+        secondWindow.postMessage({newTime: seconds}, '*');
+        secondWindow.postMessage('resetMatch', '*');
+    }
+    updateScore();
+    uncheckAllCheckboxes();
+    akaippon = 0;
+    akawazaari = 0;
+    akayuko = 0;
+    aoippon = 0;
+    aowazaari = 0;
+    aoyuko = 0;
+    updatecount()
+    end.muted = true;
+    audioPlayer.muted = true;
+    document.getElementById('akasenshu').style.backgroundColor = 'rgb(223, 223, 223)';
+    document.getElementById('aosenshu').style.backgroundColor = 'rgb(223, 223, 223)';
+    document.getElementById("aoipponcount").setAttribute("disabled", "disabled");
+    document.getElementById("akaipponcount").setAttribute("disabled", "disabled");
+    document.getElementById("aowazaaricount").setAttribute("disabled", "disabled");
+    document.getElementById("akawazaaricount").setAttribute("disabled", "disabled");
+    document.getElementById("aoyukocount").setAttribute("disabled", "disabled");
+    document.getElementById("akayukocount").setAttribute("disabled", "disabled");
+}
+
+function updateTimer() {
+    seconds--;
+    if (seconds < 0) {
+        pauseTimer();
+        seconds = 0;
+        end.muted = false;
+        end.play()
+    } else if (seconds <= 15) {
+        document.getElementById('timerDisplay').style.color = 'rgb(255, 0, 0)';
+    }
+    if (seconds === 15) {
+        audioPlayer.muted = false;
+        audioPlayer.play()
+    }
+    document.getElementById('timerDisplay').innerText = formatTime(seconds);
+}
+
+function updateTimerr(newTime) {
+    seconds = newTime;
+    if (!timerRunning) {
+        document.getElementById('timerDisplay').innerText = formatTime(seconds);
+    }
+}
+
+
+// Обработчик события перед закрытием первого окна
+window.addEventListener("beforeunload", function(event) {
+    if (secondWindow && !secondWindow.closed) {
+        secondWindow.postMessage('close', '*');
+    }
+});
+
+// Функция для обновления времени на втором окне
+function updateTime() {
+    let minutes = parseInt(document.getElementById('minutes').value);
+    let seconds = parseInt(document.getElementById('seconds').value);
+
+    if (!isNaN(minutes) && !isNaN(seconds)) {
+        seconds = minutes * 60 + seconds;
+        if (secondWindow && !secondWindow.closed) {
+            secondWindow.postMessage({newTime: seconds}, '*');
+        }
+        updateTimerr(seconds);
+        document.getElementById("time-modal").style.display = "none";
+    }
+}
+
+function updateTatami() {
+    let number = parseInt(document.getElementById('tatami-number').value);
+
+    if (!isNaN(number)) {
+        if (secondWindow && !secondWindow.closed) {
+            secondWindow.postMessage({newTatami: number}, '*');
+        }
+    }
+    document.getElementById("tatami-modal").style.display = "none";
+}
+
+function showAkaSenshu() {
+    AkaVisible = !AkaVisible;
+    document.getElementById('akasenshu').style.backgroundColor = AkaVisible ? 'rgb(255, 238, 0)' : 'rgb(223, 223, 223)';
+    if (secondWindow && !secondWindow.closed) {
+        secondWindow.postMessage({ showAkaSenshu: true }, '*');
+    }
+}
+
+function showAoSenshu() {
+    AoVisible = !AoVisible;
+    document.getElementById('aosenshu').style.backgroundColor = AoVisible ? 'rgb(255, 238, 0)' : 'rgb(223, 223, 223)';
+    if (secondWindow && !secondWindow.closed) {
+        secondWindow.postMessage({ showAoSenshu: true }, '*');
+    }
+}
---
+++ js/scoreboard.js
@@ -0,0 +1,227 @@
+let timerInterval;
+let timerRunning = false;
+let seconds = 90;
+let senshuAkaVisible = false;
+let senshuAoVisible = false;
+var akaScore = 0;
+var aoScore = 0;
+
+function warn(pen) {
+    if (pen === 'akac1') {
+        document.getElementById('akac1').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('akac1').style.borderRadius = '13px';
+        document.getElementById('akac1').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'akac2') {
+        document.getElementById('akac2').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('akac2').style.borderRadius = '13px';
+        document.getElementById('akac2').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'akac3') {
+        document.getElementById('akac3').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('akac3').style.borderRadius = '13px';
+        document.getElementById('akac3').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'akahc') {
+        document.getElementById('akahc').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('akahc').style.borderRadius = '13px';
+        document.getElementById('akahc').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'akah') {
+        document.getElementById('akah').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('akah').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+        document.getElementById('akah').style.borderRadius = '13px';
+        document.getElementById('ao-score').classList.add('flashing');
+    } else if (pen === 'aoc1') {
+        document.getElementById('aoc1').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('aoc1').style.borderRadius = '13px';
+        document.getElementById('aoc1').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'aoc2') {
+        document.getElementById('aoc2').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('aoc2').style.borderRadius = '13px';
+        document.getElementById('aoc2').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'aoc3') {
+        document.getElementById('aoc3').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('aoc3').style.borderRadius = '13px';
+        document.getElementById('aoc3').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'aohc') {
+        document.getElementById('aohc').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('aohc').style.borderRadius = '13px';
+        document.getElementById('aohc').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+    } else if (pen === 'aoh') {
+        document.getElementById('aoh').style.border = '5px solid rgb(255, 238, 0)';
+        document.getElementById('aoh').style.borderRadius = '13px';
+        document.getElementById('aoh').style.backgroundColor = 'rgba(255, 217, 0, 0.342)';
+        document.getElementById('aka-score').classList.add('flashing');
+    }
+}
+
+function unwarn(pen) {
+    if (pen === 'akac1') {
+        document.getElementById('akac1').style.border = 'none';
+        document.getElementById('akac1').style.backgroundColor = 'transparent';
+    } else if (pen === 'akac2') {
+        document.getElementById('akac2').style.border = 'none';
+        document.getElementById('akac2').style.backgroundColor = 'transparent';
+    } else if (pen === 'akac3') {
+        document.getElementById('akac3').style.border = 'none';
+        document.getElementById('akac3').style.backgroundColor = 'transparent';
+    } else if (pen === 'akahc') {
+        document.getElementById('akahc').style.border = 'none';
+        document.getElementById('akahc').style.backgroundColor = 'transparent';
+    } else if (pen === 'akah') {
+        document.getElementById('akah').style.border = 'none';
+        document.getElementById('akah').style.backgroundColor = 'transparent';
+    } else if (pen === 'aoc1') {
+        document.getElementById('aoc1').style.border = 'none';
+        document.getElementById('aoc1').style.backgroundColor = 'transparent';
+    } else if (pen === 'aoc2') {
+        document.getElementById('aoc2').style.border = 'none';
+        document.getElementById('aoc2').style.backgroundColor = 'transparent';
+    } else if (pen === 'aoc3') {
+        document.getElementById('aoc3').style.border = 'none';
+        document.getElementById('aoc3').style.backgroundColor = 'transparent';
+    } else if (pen === 'aohc') {
+        document.getElementById('aohc').style.border = 'none';
+        document.getElementById('aohc').style.backgroundColor = 'transparent';
+    } else if (pen === 'aoh') {
+        document.getElementById('aoh').style.border = 'none';
+        document.getElementById('aoh').style.backgroundColor = 'transparent';
+    }
+}
+
+function unwarnall() {
+    document.getElementById('akac1').style.border = 'none';
+    document.getElementById('akac2').style.border = 'none';
+    document.getElementById('akac3').style.border = 'none';
+    document.getElementById('akahc').style.border = 'none';
+    document.getElementById('akah').style.border = 'none';
+    document.getElementById('aoc1').style.border = 'none';
+    document.getElementById('aoc2').style.border = 'none';
+    document.getElementById('aoc3').style.border = 'none';
+    document.getElementById('aohc').style.border = 'none';
+    document.getElementById('aoh').style.border = 'none';
+
+    document.getElementById('akac1').style.backgroundColor = 'transparent';
+    document.getElementById('akac2').style.backgroundColor = 'transparent';
+    document.getElementById('akac3').style.backgroundColor = 'transparent';
+    document.getElementById('akahc').style.backgroundColor = 'transparent';
+    document.getElementById('akah').style.backgroundColor = 'transparent';
+    document.getElementById('aoc1').style.backgroundColor = 'transparent';
+    document.getElementById('aoc2').style.backgroundColor = 'transparent';
+    document.getElementById('aoc3').style.backgroundColor = 'transparent';
+    document.getElementById('aohc').style.backgroundColor = 'transparent';
+    document.getElementById('aoh').style.backgroundColor = 'transparent';
+}
+
+// Функция для запуска таймера
+function startTimer() {
+    if (!timerRunning) {
+        timerInterval = setInterval(updateTimer, 1000);
+        timerRunning = true;
+        document.getElementById('timer').style.color = 'rgb(255, 187, 0)';
+    }
+}
+
+// Функция для остановки таймера
+function pauseTimer() {
+    clearInterval(timerInterval);
+    timerRunning = false;
+    document.getElementById('timer').style.color = 'white';
+}
+
+// Функция для обновления таймера
+function updateTimer() {
+    seconds--;
+    if (seconds < 0) {
+        pauseTimer();
+        seconds = 0;
+        if (akaScore < aoScore) {
+            document.getElementById('ao-score').classList.add('flashing');
+        } else if (akaScore > aoScore) {
+            document.getElementById('aka-score').classList.add('flashing');
+        } else if (akaScore === aoScore) {
+            if (senshuAkaVisible === true) {
+                document.getElementById('aka-score').classList.add('flashing');
+            } else if (senshuAoVisible === true) {
+                document.getElementById('ao-score').classList.add('flashing');
+            }
+        }
+    } else if (seconds <= 15) {
+        document.getElementById('timer').style.color = 'rgb(255, 0, 0)';
+    }
+    document.getElementById('timer').innerText = formatTime(seconds);
+}
+
+// Функция для форматирования времени
+function formatTime(time) {
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = time % 60;
+
+    return `${minutes < 10 ? minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+}
+
+window.addEventListener('message', function(event) {
+    if (event.data === 'start') { // старт времени
+        startTimer();
+    } else if (event.data === 'pause') { // пауза времени
+        pauseTimer();
+    } else if (event.data === 'close') { // закрытие окна
+        closeWindow();
+    } else if (event.data.newTime !== undefined) { // смена времени
+        updateTime(event.data.newTime);
+    } else if (event.data.newTatami !== undefined) { // смена номера татами
+        updateTatami(event.data.newTatami);
+    } else if (event.data.showAkaSenshu) { // ака сеншу
+        senshuAkaVisible = !senshuAkaVisible;
+        document.getElementById('aka-senshu').style.display = senshuAkaVisible ? 'flex' : 'none';
+    } else if (event.data === 'resetMatch') { // Перезапустить матч
+        senshuAkaVisible = false
+        document.getElementById('aka-senshu').style.display = 'none';
+        senshuAoVisible = false
+        document.getElementById('ao-senshu').style.display = 'none';
+        unwarnall();
+        akaScore = 0;
+        aoScore = 0;
+    } else if (event.data.showAoSenshu) { // ао сеншу
+        senshuAoVisible = !senshuAoVisible;
+        document.getElementById('ao-senshu').style.display = senshuAoVisible ? 'flex' : 'none';
+    } else if (event.data.newScore) { // обновить счет
+        newAkaScore = event.data.newScore.newAkaScore;
+        newAoScore = event.data.newScore.newAoScore;
+        document.getElementById('aka-score').innerText = newAkaScore;
+        document.getElementById('ao-score').innerText = newAoScore;
+        akaScore = newAkaScore;
+        aoScore = newAoScore;
+
+        var scoreDifference = Math.abs(newAkaScore - newAoScore);
+        if (scoreDifference >= 8) {
+            if (newAkaScore > newAoScore) {
+                document.getElementById('aka-score').classList.add('flashing');
+            } else if (newAkaScore < newAoScore) {
+                document.getElementById('ao-score').classList.add('flashing');
+            }       
+        } else {
+            document.getElementById('aka-score').classList.remove('flashing');
+            document.getElementById('ao-score').classList.remove('flashing');
+        }
+    } else if (event.data.newWarn) { // выдать наказание
+        warn(event.data.newWarn);
+    } else if (event.data.unWarn) { // снять наказание
+        unwarn(event.data.unWarn);
+    }
+});
+
+// Функция для обновления времени
+function updateTime(newTime) {
+    seconds = newTime;
+    if (!timerRunning) {
+        document.getElementById('timer').innerText = formatTime(seconds);
+    }
+}
+
+function updateTatami(newTatami) {
+    number = newTatami;
+    document.getElementById('tatami-number').innerText = number;
+}
+
+// Функция для закрытия окна
+function closeWindow() {
+    window.close();
+}
